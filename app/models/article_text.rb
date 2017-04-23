2a57119ad9f29c4c31b4c6079a41782529908a79
Brandon Walter <bs.walter91@gmail.com>
2017-04-23 15:57:16 -0700

    aer3r

--- app/models/article_text.rb
+++ app/models/article_text.rb
@@ -2,11 +2,11 @@
 class ArticleText < ApplicationRecord
 	# include Elasticsearch::Model
  #  	include Elasticsearch::Model::Callbacks
-
-	# settings index: { number_of_shards: 1 } do
-	#     mappings dynamic: 'false' do
-	#       indexes :title, analyzer: 'english', index_options: 'offsets'
-	#     end
- #  	end
+ 
+	settings index: { number_of_shards: 1 } do
+	    mappings dynamic: 'false' do
+	      indexes :title, analyzer: 'english', index_options: 'offsets'
+	    end
+  	end
 
 end
