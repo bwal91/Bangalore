1b0578fd9f4e3bbfcc165f8c2a886fb91353cd94
Brandon Walter <bs.walter91@gmail.com>
2017-04-27 18:21:02 -0700

    comeonnn

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require_relative 'boot'
 require 'rails/all'
 require 'sprockets/railtie'
 require 'v8'
+require 'uglifier'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +11,7 @@ ENV['RAILS_ADMIN_THEME'] = 'rollincode'
 module Bangalore
   class Application < Rails::Application
 
-	config.serve_static_assets = true
+	# config.serve_static_assets = true
   	config.time_zone = 'Mumbai'
 
     config.autoload_paths += %W(#{config.root}/lib)
